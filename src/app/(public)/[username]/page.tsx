d54f533cdd9d108b012471c155d6819425dd89ce
neathcmd <ksdf71464@gmail.com>
2025-07-21 16:43:27 +0700

    not working yet

--- src/app/(public)/[username]/page.tsx
+++ src/app/(public)/[username]/page.tsx
@@ -11,6 +11,7 @@ const Page = async ({ params }: { params: Promise<{ username: string }> }) => {
   if (!data.ok) {
     // Handle HTTP errors
     throw new Error(`Failed to fetch cards: ${data.status}`);
+    // console.log("Failed to fetch cards:", data.status);
   }
 
   const cards: CardResponse = await data.json();
